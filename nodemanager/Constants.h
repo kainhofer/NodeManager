e4ead07d18057c9d3d7a707ed132f3c68f32cc5c
Reinhold Kainhofer <reinhold@kainhofer.com>
2018-10-08 01:19:05 +0200

    Improve support for NRF5 boards...
    -) Allow other boards than the MyBoardNRF5*
    -) use the generic SS1306 wire library rather than the AVR-specific version
    -) the nrf5* has disconnected high output mode, so use this together with the sensor's pullup to prevent frying the VL53L0X laser distance sensor
    -) prevent crash if _lox==NULL in VL53L0X
    
    Signed-off-by: Reinhold Kainhofer <reinhold@kainhofer.com>

--- nodemanager/Constants.h
+++ nodemanager/Constants.h
@@ -86,7 +86,7 @@ Chip type
 #if defined(ESP8266) || defined(MY_GATEWAY_ESP8266)
 #define CHIP_ESP8266
 #endif
-#if defined (MYBOARDNRF5)
+#if defined (MYBOARDNRF5) || defined(NRF51) || defined(NRF52)
 #define CHIP_NRF5
 #endif
 #if !defined(CHIP_ESP8266) && !defined(CHIP_STM32) && !defined(CHIP_NRF5)
